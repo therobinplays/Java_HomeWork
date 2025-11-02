35c0619cb9329e1d5db8b2faf65f2b3a7eefb5bc
ᖇᴏʙɪɴ ᑭʟᴀʏꜱ <therealrobinplays@gmail.com>
2025-11-02 19:06:44 +0545

    Improve input prompts in StudentDetail.java
    Updated prompts for user input to improve formatting.

--- Homework_3/StudentDetail.java
+++ Homework_3/StudentDetail.java
@@ -1,17 +1,17 @@
 // A Program for Displaying Name,Class and Address.
 
 import java.util.Scanner;
-public class Main
+public class StudentDetail
 {
     public static void main (String[] args)
     {
         System.out.println("----Enter your Detail----");
         
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter Your Name:");
+        System.out.println("Enter Your Name : ");
         String name = scan.nextLine();
         
-        System.out.println("Enter Your Class:");
+        System.out.println("Enter Your Class : ");
         int stdclass= scan.nextInt();
         scan.nextLine();
         
