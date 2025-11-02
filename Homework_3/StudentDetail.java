b80760fb51ff910006fc86edbebff82063484db7
ᖇᴏʙɪɴ ᑭʟᴀʏꜱ <therealrobinplays@gmail.com>
2025-11-02 18:54:21 +0545

    added

---
+++ Homework_3/StudentDetail.java
@@ -0,0 +1,31 @@
+// A Program for Displaying Name,Class and Address.
+
+import java.util.Scanner;
+public class Main
+{
+    public static void main (String[] args)
+    {
+        System.out.println("----Enter your Detail----");
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Your Name:");
+        String name = scan.nextLine();
+        
+        System.out.println("Enter Your Class:");
+        int stdclass= scan.nextInt();
+        scan.nextLine();
+        
+        System.out.println("Enter your Address : ");
+        String address = scan.nextLine();
+        
+        System.out.println("----Student Detail----");
+        
+        System.out.println("Your Name is : " + name);
+        System.out.println("You Study in Class : " + stdclass);
+        System.out.println("Your Live in:" + address);
+        
+        scan.close();
+        
+        
+    }
+}
