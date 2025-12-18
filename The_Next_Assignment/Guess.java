2fc5c96e5149d544e29deb85bc0fedd7d75525a0
ᖇᴏʙɪɴ ᑭʟᴀʏꜱ <therealrobinplays@gmail.com>
2025-12-18 08:56:43 +0545

    improved

--- The_Next_Assignment/Guess.java
+++ The_Next_Assignment/Guess.java
@@ -2,73 +2,77 @@ import java.util.Scanner;
 
 public class Guess {
     int value;
-    int count=0;
+    int count = 0;
 
-    Guess(int value) 
-    {
+    Guess(int value) {
         this.value = value;
-    System.out.println("----- Mr. Integer Finder -----");
+        System.out.println("----- Mr. Integer Finder -----");
     }
-    
-    
-    void check() 
-    {
+
+    void check() {
         Scanner scan = new Scanner(System.in);
 
-        while (true) 
-        {
+        while (true) {
             System.out.print("Enter the secret number: ");
             int userInput = scan.nextInt();
             count++;
 
-            if (userInput == 30) 
+            if (userInput == value) 
             {
-                System.out.println("Your provided number " + userInput + " is double of the Secret Number.");
-            }
-
-            else if (userInput >= 16 && userInput <= 20) 
-            {
-                System.out.println("Your provided number " + userInput + " is very close, but greater than the Secret Number.");
-            }
-
-            else if (userInput >= 21 && userInput <= 50)
-            {
-                System.out.println("Your provided number " + userInput +" is greater than the Secret Number.");
-            }
-
-            else if (userInput >= 51 && userInput <= 100)
-            {
-                System.out.println("Your provided number " + userInput + " is too greater than the Secret Number.");
-            }
-
-            else if (userInput > 100)
-            {
-                System.out.println("Your provided number " + userInput + " is out of range, too greater than the Secret Number.");
-            }
-
-            else if (userInput <= 0)
-            {
-                System.out.println("Your provided number " + userInput +" is too smaller than the Secret Number.");
+                System.out.println("You guessed the Secret Number " + value +
+                        " correctly!");
+                break;
             }
             
-            else if (userInput >= 1 && userInput <= 10) 
+            else if (userInput == value * 2) 
             {
-                System.out.println("Your provided number " + userInput + " is smaller than the Secret Number.");
-            } 
-
-            else if(userInput >= 11 && userInput <= 14)
+                System.out.println("Your provided number " + userInput +
+                        " is double of the Secret Number.");
+            }
+            
+            else if (userInput > value + 20) 
             {
-                System.out.println("Your provided number " + userInput +" is very close, but smaller than the Secret Number.");
+                System.out.println("Your provided number " + userInput +
+                        " is too greater than the Secret Number.");
+            }
+            
+            else if (userInput > value) 
+            {
+                if (userInput <= value + 5) 
+                {
+                    System.out.println("Your provided number " + userInput +
+                            " is very close, but greater than the Secret Number.");
+                } 
+                
+                else 
+                {
+                    System.out.println("Your provided number " + userInput +
+                            " is greater than the Secret Number.");
+                }
+            }
+            
+            else if (userInput < value - 20) 
+            {
+                System.out.println("Your provided number " + userInput +
+                        " is too smaller than the Secret Number.");
             }
             
             else 
             {
-                System.out.println("You guess that the Secret Number is " + userInput + " , thats absolutely correct !");
-                break;
+                if (userInput >= value - 5) {
+                    System.out.println("Your provided number " + userInput +
+                            " is very close, but smaller than the Secret Number.");
+                } 
+                
+                else 
+                {
+                    System.out.println("Your provided number " + userInput +
+                            " is smaller than the Secret Number.");
+                }
             }
         }
-        
-        System.out.println("You have Guess the Secret Number in " + count + " Attempts.");
+
+        System.out.println("You guessed the Secret Number in " + count + " attempts.");
         scan.close();
     }
 
@@ -77,3 +81,4 @@ public class Guess {
         g.check();
     }
 }
+
